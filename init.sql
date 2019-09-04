372c74709e4c95b971b9166362ba7eea97a15696
TheC <i@thec.me>
2019-09-04 01:55:19 -0700

    update sql structure

--- init.sql
+++ init.sql
@@ -1,11 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 27, 2014 at 06:13 AM
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `pixiv_rss`
@@ -14,7 +12,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `award_log`
+-- 表的结构 `award_log`
 --
 
 CREATE TABLE `award_log` (
@@ -26,11 +24,41 @@ CREATE TABLE `award_log` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pixiv_weibo_id_map`
+-- 表的结构 `pixiv_weibo_id_map`
 --
 
 CREATE TABLE `pixiv_weibo_id_map` (
   `pixiv_uid` int(8) NOT NULL,
-  `weibo_uid` varchar(32) NOT NULL,
-  PRIMARY KEY (`pixiv_uid`)
+  `weibo_uid` varchar(32) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Pixiv_id与Weibo_id的映射表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `weibo_post_history`
+--
+
+CREATE TABLE `weibo_post_history` (
+  `pixiv_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pixiv_weibo_id_map`
+--
+ALTER TABLE `pixiv_weibo_id_map`
+  ADD PRIMARY KEY (`pixiv_uid`);
+
+--
+-- Indexes for table `weibo_post_history`
+--
+ALTER TABLE `weibo_post_history`
+  ADD PRIMARY KEY (`pixiv_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
