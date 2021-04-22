15fa289f88741919f32036ecb75a6c17be5407cf
TheC <i@thec.me>
2021-04-21 18:49:43 -0700

    does heartbeat come from /config?

--- weibo.py
+++ weibo.py
@@ -3,54 +3,53 @@ from utility import *
 
 from math import floor
 
-# load local cookies
-f = open(WEIBO_COOKIE_FILE, 'r')
-cookies = json.load(f)
-f.close()
-
-originalCookieStr = json.dumps(cookies)
-
-# prepare requests, fill in the previous cookies
-s = requests.Session()
-s.cookies.update(cookies)
-
-# heartbeat
-def heartbeat():
-    global cookies, originalCookieStr, s
-    # prepare headers
-    headers = {
-        'mweibo-pwa': "1",
-        'referer': 'https://m.weibo.cn/sw.js',
-        'x-requested-with': 'XMLHttpRequest',
-        'x-xsrf-token': cookies['XSRF-TOKEN'],
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
-    }
-    s.headers = headers
-
-    # get current timestamp, to ms as weibo implements
-    ts = floor(time.time() * 1000)
-
-    # send request
-    r = s.get('https://m.weibo.cn/api/remind/unread?t=' + str(ts))
-
-    # check if return is ok
-    data = r.json()
-    if data['ok'] != 1:
-        log('Weibo heartbeat refresh faild!')
-        log('--- Sent cookie')
-        log(originalCookieStr)
-        log('--- Response:')
-        log(r.text)
-        log('--- Return Cookie:')
-        log(r.cookies)
-    else:
-        # save updated cookies
-        f = open(WEIBO_COOKIE_FILE, 'w')
-        cookies = json.dumps(s.cookies.get_dict())
-        f.write(cookies)
+class Weibo():
+    def __init__(self):
+        # load local cookies
+        f = open(WEIBO_COOKIE_FILE, 'r')
+        self.cookies = json.load(f)
         f.close()
-        debug('Weibo heartbeat refresh succeed')
+
+        self.originalCookieStr = json.dumps(self.cookies)
+
+        # prepare requests, fill in the previous cookies
+        self.s = requests.Session()
+        self.s.cookies.update(self.cookies)
+
+    # heartbeat
+    def heartbeat(self):
+        # prepare headers
+        headers = {
+            'mweibo-pwa': "1",
+            'referer': 'https://m.weibo.cn/sw.js',
+            'x-requested-with': 'XMLHttpRequest',
+            'x-xsrf-token': self.cookies['XSRF-TOKEN'],
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
+        }
+        self.s.headers = headers
+
+        # send request
+        r = self.s.get('https://m.weibo.cn/api/config')
+
+        # check if return is ok
+        data = r.json()
+        if data['ok'] != 1:
+            log('Weibo heartbeat refresh faild!')
+            log('--- Sent cookie')
+            log(self.originalCookieStr)
+            log('--- Response:')
+            log(r.text)
+            log('--- Return Cookie:')
+            log(r.cookies)
+        else:
+            # save updated cookies
+            f = open(WEIBO_COOKIE_FILE, 'w')
+            cookies = json.dumps(self.s.cookies.get_dict())
+            f.write(cookies)
+            f.close()
+            debug('Weibo heartbeat refresh succeed')
 
 # when invoked from terminal, call heartbeat
 if __name__ == '__main__':
-    heartbeat()
+    weibo = Weibo()
+    weibo.heartbeat()
